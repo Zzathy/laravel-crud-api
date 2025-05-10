18d878bb2cf86fe84a2b5121500d64f7b7fbeb58
Zzathy <izzafathony27@gmail.com>
2025-05-11 03:45:03 +0700

    feat(category): prepare the category before CRUD

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    protected $fillable = ['name'];
     use HasFactory;
+    use HasUlids;
 }
