5910d5e9ce4ee608e6463bfcfab9cb96e093da22
Zzathy <izzafathony27@gmail.com>
2025-05-12 20:09:56 +0700

    feat(category): finish CRD, the update is error on postman

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -14,7 +17,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return ProductResource::collection(Product::all());
     }
 
     /**
@@ -22,7 +25,27 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request)
     {
-        //
+        try {
+            $filename = null;
+
+            if ($request->image) {
+                $image = $request->image;
+                $filename = Carbon::now()->format('YmdHis') . '.' . $image->extension();
+                $image->storeAs('images', $filename, 'public');
+            }
+
+            $product = Product::create([
+                'name' => $request->name,
+                'category_id' => $request->category_id,
+                'description' => $request->description,
+                'price' => $request->price,
+                'image' => $filename
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(null, 500);
+        }
+
+        return ProductResource::make($product);
     }
 
     /**
@@ -30,7 +53,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return ProductResource::make($product);
     }
 
     /**
@@ -38,7 +61,30 @@ class ProductController extends Controller
      */
     public function update(UpdateProductRequest $request, Product $product)
     {
-        //
+        try {
+            if ($request->hasFile('image')) {
+                if ($product->image) {
+                    Storage::disk('public')->delete('images/' . $product->image);
+                }
+
+                $image = $request->image;
+                $filename = Carbon::now()->format('YmdHis') . '.' . $image->extension();
+                $image->storeAs('images', $filename, 'public');
+
+                $product->image = $filename;
+            }
+
+            $product->name = $request->name;
+            $product->category_id = $request->category_id;
+            $product->description = $request->description;
+            $product->price = $request->price;
+
+            $product->save();
+
+            return ProductResource::make($product);
+        } catch (\Exception $e) {
+            return response()->json(null, 500);
+        }
     }
 
     /**
@@ -46,6 +92,16 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        try {
+            if ($product->image) {
+                Storage::disk('public')->delete('images/' . $product->image);
+            }
+
+            $product->delete();
+            
+            return response()->json(null, 204);
+        } catch (\Exception $e) {
+            return response()->json(null, 500);
+        }
     }
 }
--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -11,7 +11,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string',
+            'category_id' => 'required|exists:categories,id',
+            'description' => 'nullable|string',
+            'price' => 'required|integer',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg'
         ];
     }
 }
